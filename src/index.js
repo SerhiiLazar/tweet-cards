cd6664d8ef2ca34f69ba00fde3528f0ee04a7e32
Serhii Lazarenok <serhiilazar90@gmail.com>
2023-04-28 23:59:39 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+// import {store} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <BrowserRouter basename="/tweet-cards">
       <App />
       </BrowserRouter>
